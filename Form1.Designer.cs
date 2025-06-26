34a6f867bb2ca8b85172c5c02ca87cd744b6ab8a
AlicanKirma <alicankirma90@hotmail.com>
2025-06-26 15:49:41 +0300

    llk paylaşım

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿using System.Windows.Forms;
+
+namespace StokEkstre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonGetir = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.maskedTextBoxBas = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBoxBit = new System.Windows.Forms.MaskedTextBox();
+            this.buttonBas = new System.Windows.Forms.Button();
+            this.buttonBit = new System.Windows.Forms.Button();
+            this.dateTimePickerBas = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerBit = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ürün";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Başlangıç";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(659, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bitiş";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(937, 390);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // buttonGetir
+            // 
+            this.buttonGetir.Location = new System.Drawing.Point(856, 15);
+            this.buttonGetir.Name = "buttonGetir";
+            this.buttonGetir.Size = new System.Drawing.Size(96, 23);
+            this.buttonGetir.TabIndex = 7;
+            this.buttonGetir.Text = "Getir";
+            this.buttonGetir.UseVisualStyleBackColor = true;
+            this.buttonGetir.Click += new System.EventHandler(this.buttonGetir_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(53, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(313, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // maskedTextBoxBas
+            // 
+            this.maskedTextBoxBas.Location = new System.Drawing.Point(469, 16);
+            this.maskedTextBoxBas.Mask = "00/00/0000";
+            this.maskedTextBoxBas.Name = "maskedTextBoxBas";
+            this.maskedTextBoxBas.Size = new System.Drawing.Size(100, 22);
+            this.maskedTextBoxBas.TabIndex = 11;
+            this.maskedTextBoxBas.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskedTextBoxBit
+            // 
+            this.maskedTextBoxBit.Location = new System.Drawing.Point(697, 16);
+            this.maskedTextBoxBit.Mask = "00/00/0000";
+            this.maskedTextBoxBit.Name = "maskedTextBoxBit";
+            this.maskedTextBoxBit.Size = new System.Drawing.Size(100, 22);
+            this.maskedTextBoxBit.TabIndex = 12;
+            this.maskedTextBoxBit.ValidatingType = typeof(System.DateTime);
+            // 
+            // buttonBas
+            // 
+            this.buttonBas.Location = new System.Drawing.Point(575, 15);
+            this.buttonBas.Name = "buttonBas";
+            this.buttonBas.Size = new System.Drawing.Size(21, 23);
+            this.buttonBas.TabIndex = 13;
+            this.buttonBas.Text = "📅";
+            this.buttonBas.UseVisualStyleBackColor = true;
+            this.buttonBas.Click += new System.EventHandler(this.buttonBas_Click);
+            // 
+            // buttonBit
+            // 
+            this.buttonBit.Location = new System.Drawing.Point(803, 15);
+            this.buttonBit.Name = "buttonBit";
+            this.buttonBit.Size = new System.Drawing.Size(23, 23);
+            this.buttonBit.TabIndex = 14;
+            this.buttonBit.Text = "📅";
+            this.buttonBit.UseVisualStyleBackColor = true;
+            this.buttonBit.Click += new System.EventHandler(this.buttonBit_Click);
+            // 
+            // dateTimePickerBas
+            // 
+            this.dateTimePickerBas.Location = new System.Drawing.Point(418, 275);
+            this.dateTimePickerBas.Name = "dateTimePickerBas";
+            this.dateTimePickerBas.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerBas.TabIndex = 15;
+            this.dateTimePickerBas.Visible = false;
+            this.dateTimePickerBas.CloseUp += new System.EventHandler(this.dateTimePickerBas_CloseUp);
+            // 
+            // dateTimePickerBit
+            // 
+            this.dateTimePickerBit.Location = new System.Drawing.Point(418, 332);
+            this.dateTimePickerBit.Name = "dateTimePickerBit";
+            this.dateTimePickerBit.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerBit.TabIndex = 16;
+            this.dateTimePickerBit.Visible = false;
+            this.dateTimePickerBit.CloseUp += new System.EventHandler(this.dateTimePickerBit_CloseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 450);
+            this.Controls.Add(this.dateTimePickerBit);
+            this.Controls.Add(this.dateTimePickerBas);
+            this.Controls.Add(this.buttonBit);
+            this.Controls.Add(this.buttonBas);
+            this.Controls.Add(this.maskedTextBoxBit);
+            this.Controls.Add(this.maskedTextBoxBas);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonGetir);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Stok Ekstre";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonGetir;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxBas;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxBit;
+        private System.Windows.Forms.Button buttonBas;
+        private System.Windows.Forms.Button buttonBit;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBas;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace StokEkstre
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Server=.;Database=Test;Trusted_Connection=True;");
+
+        private void Urunler()
+        {
+            try
+            {               
+                con.Open();
+                string kayit = "sp_StokUrunler";
+                SqlCommand komut = new SqlCommand(kayit, con);
+                komut.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(komut);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                comboBox1.DataSource = dt;
+                comboBox1.DisplayMember = "MalAdi";
+                comboBox1.ValueMember = "MalKodu";
+                con.Close();
+            }
+            catch
+            {
+
+                MessageBox.Show("Veri yükleme sırasında hata oluştu");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Urunler();
+            dateTimePickerBas.Format = DateTimePickerFormat.Short;
+            dateTimePickerBas.Visible = false;
+
+            // Takvimi MaskedTextBox'ın altına konumla
+            dateTimePickerBas.Location = new Point(maskedTextBoxBas.Left, maskedTextBoxBas.Bottom + 2);
+
+            dateTimePickerBit.Format = DateTimePickerFormat.Short;
+            dateTimePickerBit.Visible = false;
+
+            // Takvimi MaskedTextBox'ın altına konumla
+            dateTimePickerBit.Location = new Point(maskedTextBoxBit.Left, maskedTextBoxBit.Bottom + 2);
+
+     
+
+        }
+
+        private void dateTimePickerBas_CloseUp(object sender, EventArgs e)
+        {
+            maskedTextBoxBas.Text = dateTimePickerBas.Value.ToString("dd/MM/yyyy");
+            dateTimePickerBas.Visible = false;
+        }
+
+        private void dateTimePickerBit_CloseUp(object sender, EventArgs e)
+        {
+            maskedTextBoxBit.Text = dateTimePickerBit.Value.ToString("dd/MM/yyyy");
+            dateTimePickerBit.Visible = false;
+        }
+
+        private void buttonGetir_Click(object sender, EventArgs e)
+        {
+            StokEkstre();
+        }
+
+        private void StokEkstre()
+        {
+            try
+            {
+                if (comboBox1.SelectedValue == null)
+                {
+                    MessageBox.Show("Lütfen bir ürün seçin!");
+                    return;
+                }
+                con.Open();
+                string kayit = "sp_StokEkstre";
+                SqlCommand komut = new SqlCommand(kayit, con);
+                komut.CommandType = CommandType.StoredProcedure;
+                DateTime bastrh = Convert.ToDateTime(maskedTextBoxBas.Text);
+                DateTime bittrh = Convert.ToDateTime(maskedTextBoxBit.Text);
+                int bastrhInt = (int)Math.Floor(bastrh.AddDays(-2).ToOADate()) + 2;
+                int bittrhInt = (int)Math.Floor(bittrh.ToOADate()) + 2;
+                SqlDataAdapter da = new SqlDataAdapter(komut);
+                string urun = comboBox1.SelectedValue?.ToString();
+                da.SelectCommand.Parameters.AddWithValue("@malkodu", urun);
+                da.SelectCommand.Parameters.AddWithValue("@bastrh", bastrhInt);
+                da.SelectCommand.Parameters.AddWithValue("@bittrh", bittrhInt);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dt.Columns.Add("StokMiktar", typeof(int));
+                var stok = 0;
+                foreach (DataRow row in dt.Rows)
+                {
+                    int giris = Convert.ToInt32(row["GirisMiktar"]);
+                    int cikis = Convert.ToInt32(row["CikisMiktar"]);
+                    string islemTuru = row["IslemTuru"].ToString().Trim();
+                    if (islemTuru == "Giriş")
+                    {
+                        stok += giris;
+                    }
+                    else if (islemTuru == "Çıkış")
+                    {
+                        stok -= cikis;
+                    }
+               
+                    row["StokMiktar"] = stok;
+                }
+
+                dataGridView1.DataSource = dt;
+                dataGridView1.Columns["GirisMiktar"].DefaultCellStyle.Format = "N0";
+                dataGridView1.Columns["CikisMiktar"].DefaultCellStyle.Format = "N0";
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Veri yükleme sırasında hata oluştu");
+                con.Close();
+            }
+
+        }
+
+        private void buttonBas_Click(object sender, EventArgs e)
+        {
+            dateTimePickerBas.Visible = true;
+            dateTimePickerBas.BringToFront(); // Üste gelsin
+            dateTimePickerBas.Focus();
+        }
+
+        private void buttonBit_Click(object sender, EventArgs e)
+        {
+            dateTimePickerBit.Visible = true;
+            dateTimePickerBit.BringToFront(); // Üste gelsin
+            dateTimePickerBit.Focus();
+        }
+
+        private void dateTimePicker1_CloseUp(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
